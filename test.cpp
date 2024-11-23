a7839b3892c40a4a3c450af4e2351b5ac92139dd
Gray Lin <llkkqq@gmail.com>
2024-11-23 11:30:15 +0800

    c

--- test.cpp
+++ test.cpp
@@ -1,34 +1,64 @@
 #include "test.h"
+#include <gst/gst.h>
 
 using namespace std; 
-
-uint32_t crc_table[256];
 
 
 int main(int argc, char *argv[])
 {
 	xlog("%s:%d, argc:%d \n\r", __func__, __LINE__, argc);
 
-	json_object_new_object();
+	// GstElement *pipeline;
+    // GstBus *bus;
+    // GstMessage *msg;
 
-	// for (int i = 0; i < argc; ++i) {
-	// 	xlog("%s:%d, argv[%d]:%s \n\r", __func__, __LINE__, i, argv[i]);
-	// }
+    // Initialize GStreamer
+    gst_init(&argc, &argv);
 
-	// if( argc < 2 )
-	// {
-	// 	xlog("%s:%d, input more than 1 params... \n\r", __func__, __LINE__);
-	// 	getchar();
-	// 	return -1;
-	// }
+    // Create the pipeline
+    // std::string pipeline_description = "videotestsrc ! videoconvert ! autovideosink";
+    // pipeline = gst_parse_launch(pipeline_description.c_str(), nullptr);
+    // if (!pipeline) {
+    //     std::cerr << "Failed to create pipeline" << std::endl;
+    //     return -1;
+    // }
 
-	// xlog("%s:%d, ===================================== \n\r", __func__, __LINE__);
-	// // cases -------------------------------------------------
-	// if ( !strcmp(argv[1], "test") ) {
+    // // Start playing
+    // gst_element_set_state(pipeline, GST_STATE_PLAYING);
 
-	// 	xlog("%s:%d, test... \n\r", __func__, __LINE__);
+    // // Wait until error or EOS
+    // bus = gst_element_get_bus(pipeline);
+    // msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE,
+    //                                  static_cast<GstMessageType>(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
 
-	// } 
+    // // Parse message
+    // if (msg) {
+    //     GError *err;
+    //     gchar *debug_info;
+
+    //     switch (GST_MESSAGE_TYPE(msg)) {
+    //     case GST_MESSAGE_ERROR:
+    //         gst_message_parse_error(msg, &err, &debug_info);
+    //         std::cerr << "Error received from element " << GST_OBJECT_NAME(msg->src)
+    //                   << ": " << err->message << std::endl;
+    //         std::cerr << "Debugging information: " << (debug_info ? debug_info : "none") << std::endl;
+    //         g_clear_error(&err);
+    //         g_free(debug_info);
+    //         break;
+    //     case GST_MESSAGE_EOS:
+    //         std::cout << "End-Of-Stream reached." << std::endl;
+    //         break;
+    //     default:
+    //         std::cerr << "Unexpected message received." << std::endl;
+    //         break;
+    //     }
+    //     gst_message_unref(msg);
+    // }
+
+    // // Free resources
+    // gst_object_unref(bus);
+    // gst_element_set_state(pipeline, GST_STATE_NULL);
+    // gst_object_unref(pipeline);
 
 	return 0;
 }
--- test.h
+++ test.h
@@ -23,20 +23,3 @@
 #else
 #define xlog printf
 #endif
-
-//#define POLY 0x1021
-#define POLY 0x8810
-//#define POLY 0x8408
-
-//#define CRC32Const 0x04C11DB7
-#define CRC32Const 0xEDB88320
-//#define CRC32Const 0x82608EDB
-
-int Min(int a,int b, int c);
-int Fibonacci(int n, void (*f)());
-
-void make_crc_table(void);
-unsigned short crc16(unsigned char *data_p, unsigned short length);
-unsigned short crc16_ccitt(const void *buf, int len);
-uint32_t crc32(uint8_t *buf, size_t len);
-unsigned int crc(unsigned char* data, unsigned int data_len, unsigned int gen, unsigned int gen_len);
