a2d020ea77229b3c3a42106dbcc919224dd5567d
super_huang <supersuperhuang@gmail.com>
2024-11-27 17:51:42 +0800

    ..

--- test.cpp
+++ test.cpp
@@ -1,151 +1,207 @@
-#include "test.h"
+#include <errno.h>
+#include <fcntl.h>
+#include <linux/videodev2.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
-#include <gst/gst.h>
-#include <opencv2/opencv.hpp>
+uint8_t *buffer;
 
-#include <string>
-using namespace std; 
+static int xioctl(int fd, int request, void *arg)
+{
+        int r;
 
-#define TEST_GST
-#define TEST_OCV
+        do r = ioctl (fd, request, arg);
+        while (-1 == r && EINTR == errno);
 
-void gst_test() {
-
-	GstElement *pipeline;
-    GstBus *bus;
-    GstMessage *msg;
-
-    // Initialize GStreamer
-    // gst_init(&argc, &argv);
-    gst_init(nullptr, nullptr);
-
-	xlog("");
-
-	guint major, minor, micro, nano;
-    gst_version (&major, &minor, &micro, &nano);
-	xlog("%d.%d.%d.%d", major, minor, micro, nano);
-
-    // Create the pipeline
-    std::string pipeline_description = "videotestsrc ! videoconvert ! autovideosink";
-    pipeline = gst_parse_launch(pipeline_description.c_str(), nullptr);
-    if (!pipeline) {
-        // std::cerr << "Failed to create pipeline" << std::endl;
-        return;
-    }
-
-	xlog("");
-
-    // Start playing
-    gst_element_set_state(pipeline, GST_STATE_PLAYING);
-
-	xlog("");
-
-    // Wait until error or EOS
-    bus = gst_element_get_bus(pipeline);
-    msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE,
-                                     static_cast<GstMessageType>(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
-	xlog("");
-
-    // Parse message
-    if (msg) {
-        GError *err;
-        gchar *debug_info;
-
-        switch (GST_MESSAGE_TYPE(msg)) {
-        case GST_MESSAGE_ERROR:
-            gst_message_parse_error(msg, &err, &debug_info);
-            // std::cerr << "Error received from element " << GST_OBJECT_NAME(msg->src)
-            //           << ": " << err->message << std::endl;
-            // std::cerr << "Debugging information: " << (debug_info ? debug_info : "none") << std::endl;
-            g_clear_error(&err);
-            g_free(debug_info);
-            break;
-        case GST_MESSAGE_EOS:
-            // std::cout << "End-Of-Stream reached." << std::endl;
-            break;
-        default:
-            // std::cerr << "Unexpected message received." << std::endl;
-            break;
-        }
-        gst_message_unref(msg);
-    }
-
-	xlog("");
-
-    // Free resources
-    gst_object_unref(bus);
-    gst_element_set_state(pipeline, GST_STATE_NULL);
-    gst_object_unref(pipeline);
-
-	xlog("");
-
-	return;
+        return r;
 }
 
-void opencv_test() {
-	// Define the GStreamer pipeline
-    std::string pipeline = "videotestsrc ! videoconvert ! appsink";
-
-    // Open the pipeline with OpenCV
-    cv::VideoCapture cap(pipeline, cv::CAP_GSTREAMER);
-
-    if (!cap.isOpened()) {
-        return;
-    }
-
-	xlog("");
-
-	// Declare the frame variable (cv::Mat) to hold the captured frame
-    cv::Mat frame;
-
-	while (true) {
-        // Capture a frame
-        cap >> frame;
-
-        if (frame.empty()) {
-			xlog("");
-            // std::cerr << "Empty frame received. Exiting..." << std::endl;
-            break;
+int print_caps(int fd)
+{
+        struct v4l2_capability caps = {};
+        if (-1 == xioctl(fd, VIDIOC_QUERYCAP, &caps))
+        {
+                perror("Querying Capabilities");
+                return 1;
         }
 
-        // Display the frame
-        cv::imshow("GStreamer Video", frame);
+        printf( "Driver Caps:\n"
+                "  Driver: \"%s\"\n"
+                "  Card: \"%s\"\n"
+                "  Bus: \"%s\"\n"
+                "  Version: %d.%d\n"
+                "  Capabilities: %08x\n",
+                caps.driver,
+                caps.card,
+                caps.bus_info,
+                (caps.version>>16)&&0xff,
+                (caps.version>>24)&&0xff,
+                caps.capabilities);
 
-        // Break on 'q' key press
-        if (cv::waitKey(10) == 'q') {
-            break;
+
+        struct v4l2_cropcap cropcap = {0};
+        cropcap.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        if (-1 == xioctl (fd, VIDIOC_CROPCAP, &cropcap))
+        {
+                perror("Querying Cropping Capabilities");
+                return 1;
         }
-    }
 
-	xlog("");
+        printf( "Camera Cropping:\n"
+                "  Bounds: %dx%d+%d+%d\n"
+                "  Default: %dx%d+%d+%d\n"
+                "  Aspect: %d/%d\n",
+                cropcap.bounds.width, cropcap.bounds.height, cropcap.bounds.left, cropcap.bounds.top,
+                cropcap.defrect.width, cropcap.defrect.height, cropcap.defrect.left, cropcap.defrect.top,
+                cropcap.pixelaspect.numerator, cropcap.pixelaspect.denominator);
 
-	// Release resources
-    cap.release();
-    cv::destroyAllWindows();
+        int support_grbg10 = 0;
 
-	xlog("");
-	
-	return;
+        struct v4l2_fmtdesc fmtdesc = {0};
+        fmtdesc.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        char fourcc[5] = {0};
+        char c, e;
+        printf("  FMT : CE Desc\n--------------------\n");
+        while (0 == xioctl(fd, VIDIOC_ENUM_FMT, &fmtdesc))
+        {
+                strncpy(fourcc, (char *)&fmtdesc.pixelformat, 4);
+                if (fmtdesc.pixelformat == V4L2_PIX_FMT_SGRBG10)
+                    support_grbg10 = 1;
+                c = fmtdesc.flags & 1? 'C' : ' ';
+                e = fmtdesc.flags & 2? 'E' : ' ';
+                printf("  %s: %c%c %s\n", fourcc, c, e, fmtdesc.description);
+                fmtdesc.index++;
+        }
+
+        if (!support_grbg10)
+        {
+            printf("Doesn't support GRBG10.\n");
+            return 1;
+        }
+
+        struct v4l2_format fmt = {0};
+        fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        fmt.fmt.pix.width = 752;
+        fmt.fmt.pix.height = 480;
+        fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_SGRBG10;
+        fmt.fmt.pix.field = V4L2_FIELD_NONE;
+
+        if (-1 == xioctl(fd, VIDIOC_S_FMT, &fmt))
+        {
+            perror("Setting Pixel Format");
+            return 1;
+        }
+
+        strncpy(fourcc, (char *)&fmt.fmt.pix.pixelformat, 4);
+        printf( "Selected Camera Mode:\n"
+                "  Width: %d\n"
+                "  Height: %d\n"
+                "  PixFmt: %s\n"
+                "  Field: %d\n",
+                fmt.fmt.pix.width,
+                fmt.fmt.pix.height,
+                fourcc,
+                fmt.fmt.pix.field);
+        return 0;
 }
-int main(int argc, char *argv[]) {
-  xlog("");
 
-  for (int i = 0; i < argc; ++i) {
-	xlog("argv[%d]:%s", i, argv[i]);
-  }
+int init_mmap(int fd)
+{
+    struct v4l2_requestbuffers req = {0};
+    req.count = 1;
+    req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    req.memory = V4L2_MEMORY_MMAP;
 
-  if (argc < 2) {
-	xlog("to input more than 1 params...");
-    return -1;
-  }
+    if (-1 == xioctl(fd, VIDIOC_REQBUFS, &req))
+    {
+        perror("Requesting Buffer");
+        return 1;
+    }
 
-  if (!strcmp(argv[1], "test")) {
-	xlog("");
-  } else if (!strcmp(argv[1], "gst")) {
-    gst_test();
-  } else if (!strcmp(argv[1], "ocv")) {
-    opencv_test();
-  }
+    struct v4l2_buffer buf = {0};
+    buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    buf.memory = V4L2_MEMORY_MMAP;
+    buf.index = 0;
+    if(-1 == xioctl(fd, VIDIOC_QUERYBUF, &buf))
+    {
+        perror("Querying Buffer");
+        return 1;
+    }
 
-  return 0;
+    buffer = mmap (NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, buf.m.offset);
+    printf("Length: %d\nAddress: %p\n", buf.length, buffer);
+    printf("Image Length: %d\n", buf.bytesused);
+
+    return 0;
+}
+
+int capture_image(int fd)
+{
+    struct v4l2_buffer buf = {0};
+    buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    buf.memory = V4L2_MEMORY_MMAP;
+    buf.index = 0;
+    if(-1 == xioctl(fd, VIDIOC_QBUF, &buf))
+    {
+        perror("Query Buffer");
+        return 1;
+    }
+
+    if(-1 == xioctl(fd, VIDIOC_STREAMON, &buf.type))
+    {
+        perror("Start Capture");
+        return 1;
+    }
+
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(fd, &fds);
+    struct timeval tv = {0};
+    tv.tv_sec = 2;
+    int r = select(fd+1, &fds, NULL, NULL, &tv);
+    if(-1 == r)
+    {
+        perror("Waiting for Frame");
+        return 1;
+    }
+
+    if(-1 == xioctl(fd, VIDIOC_DQBUF, &buf))
+    {
+        perror("Retrieving Frame");
+        return 1;
+    }
+
+    int outfd = open("out.img", O_RDWR);
+    write(outfd, buffer, buf.bytesused);
+    close(outfd);
+
+    return 0;
+}
+
+int main()
+{
+        int fd;
+
+        fd = open("/dev/video0", O_RDWR);
+        if (fd == -1)
+        {
+                perror("Opening video device");
+                return 1;
+        }
+
+        if(print_caps(fd))
+            return 1;
+
+        if(init_mmap(fd))
+            return 1;
+
+        if(capture_image(fd))
+            return 1;
+
+        close(fd);
+        return 0;
 }
