af9a6b35aae5cc5efdeaab072abd2abee6adad8d
Gray Lin <gray.lin@primax.com.tw>
2024-11-28 17:24:45 +0800

    z

--- test.cpp
+++ test.cpp
@@ -91,14 +91,15 @@ void opencv_test() {
     // v4l2src device=${VIDEO_DEV[0]} ! video/x-raw,width=640,height=480 ! v4l2h264enc extra-controls="cid,video_gop_size=30" capture-io-mode=mmap !
 
     // std::string pipeline = "v4l2src device=/dev/video60 ! video/x-raw, width=640, height=480, framerate=30/1 ! videoconvert ! appsink";
-    // std::string pipeline = "v4l2src device=/dev/video43 ! video/x-raw, width=640, height=480, framerate=30/1 ! videoconvert  ! tee name=t \
-    //                         t. ! queue ! rtspclientsink location=rtsp://localhost:8554/mystream \
+    // std::string pipeline = "v4l2src device=/dev/video43 ! video/x-raw, width=640, height=480, framerate=30/1 ! videoconvert  ! tee name=t
+    //                         t. ! queue ! rtspclientsink location=rtsp://localhost:8554/mystream
     //                         t. ! queue ! appsink";
 
                             // ! videoconvert ! tee name=t 
                             // ! v4l2h264enc extra-controls=\"cid,video_gop_size=30\" capture-io-mode=mmap
 
-    std::string pipeline = "v4l2src device=/dev/video52 ! video/x-raw, width=640, height=480, framerate=30/1 "
+    std::string pipeline = "v4l2src device=/dev/video52 "
+                            "! video/x-raw, width=640, height=480, framerate=30/1 "
                             "! v4l2h264enc extra-controls=\"cid,video_gop_size=30\" capture-io-mode=mmap "
                             "! tee name=t "
                             "t. ! queue ! rtspclientsink location=rtsp://localhost:8554/mystream "
