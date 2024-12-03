224ebf904f1b9250e9f2d6a53a555e8d9987c737
Gray Lin <gray.lin@primax.com.tw>
2024-12-03 19:07:10 +0800

    z

--- test.cpp
+++ test.cpp
@@ -2,7 +2,27 @@
 #include "test_gst.h"
 #include "test_ocv.h"
 
-// using namespace std; 
+#include <regex>
+
+std::string getVideoDevice() {
+    std::string result;
+    FILE* pipe = popen("v4l2-ctl --list-devices | grep mtk-v4l2-camera -A 3", "r");
+    if (pipe) {
+        std::string output;
+        char buffer[256];
+        while (fgets(buffer, sizeof(buffer), pipe)) {
+            output += buffer;
+        }
+        pclose(pipe);
+
+        std::regex device_regex("/dev/video\\d+");
+        std::smatch match;
+        if (std::regex_search(output, match, device_regex)) {
+            result = match[0];
+        }
+    }
+    return result;
+}
 
 int main(int argc, char *argv[]) {
   xlog("");
@@ -19,11 +39,13 @@ int main(int argc, char *argv[]) {
   if (!strcmp(argv[1], "test")) {
     xlog("");
   } else if (!strcmp(argv[1], "gst")) {
-	int testCase = (argv[2] == nullptr) ? 0 : 1;
+    int testCase = (argv[2] == nullptr) ? 0 : 1;
     gst_test(testCase);
   } else if (!strcmp(argv[1], "ocv")) {
-	int testCase = (argv[2] == nullptr) ? 0 : 1;
+    int testCase = (argv[2] == nullptr) ? 0 : 1;
     ocv_test(testCase);
+  } else if (!strcmp(argv[1], "dev")) {
+    xlog("getVideoDevice:%s", getVideoDevice().c_str());
   }
 
   return 0;
