1aa8fc6bc6a6c640ad5618a79a1c9eeeebc007fd
Gray Lin <gray.lin@primax.com.tw>
2024-12-04 11:45:44 +0800

    z

--- test.h
+++ test.h
@@ -8,6 +8,9 @@
 #include <algorithm>
 #include <thread>
 #include <string> 
+#include <linux/videodev2.h>  // For V4L2 definitions
+#include <sys/ioctl.h>        // For ioctl()
+#include <fcntl.h>            // For open()
 
 using namespace std;
 
