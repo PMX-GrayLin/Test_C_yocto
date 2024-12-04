b760a6aae5ba2c4ff6612608d746f3874925159e
Gray Lin <gray.lin@primax.com.tw>
2024-12-04 19:13:43 +0800

    z

--- test_ocv.cpp
+++ test_ocv.cpp
@@ -6,37 +6,15 @@
 void ocv_test(int testCase) {
   xlog("testCase:%d", testCase);
 
-  // Define the GStreamer pipeline
-  // std::string pipeline = "videotestsrc ! videoconvert ! appsink";
-  // v4l2src device=/dev/video{CAM_ID} ! video/x-raw, width=640, height=480, framerate=30/1 ! videoconvert ! appsink
-  // v4l2src device=${VIDEO_DEV[0]} ! video/x-raw,width=640,height=480 ! v4l2h264enc extra-controls="cid,video_gop_size=30" capture-io-mode=mmap !
-
-  // std::string pipeline = "v4l2src device=/dev/video60 ! video/x-raw, width=640, height=480, framerate=30/1 ! videoconvert ! appsink";
-  // std::string pipeline = "v4l2src device=/dev/video43 ! video/x-raw, width=640, height=480, framerate=30/1 ! videoconvert  ! tee name=t
-  //                         t. ! queue ! rtspclientsink location=rtsp://localhost:8554/mystream
-  //                         t. ! queue ! appsink";
-
-  // ! videoconvert ! tee name=t
-  // ! v4l2h264enc extra-controls=\"cid,video_gop_size=30\" capture-io-mode=mmap
-
-  // std::string pipeline = "v4l2src device=/dev/video52 "
-  //                         "! video/x-raw, width=640, height=480, framerate=30/1 "
-  //                         "! v4l2h264enc extra-controls=\"cid,video_gop_size=30\" capture-io-mode=mmap "
-  //                         "! tee name=t "
-  //                         "t. ! queue ! rtspclientsink location=rtsp://localhost:8554/mystream "
-  //                         "t. ! queue ! appsink emit-signals=true sync=false";
-
   // OK
-  // gst-launch-1.0 \
-//   videotestsrc \
-//   ! v4l2h264enc extra-controls="cid,video_gop_size=30" capture-io-mode=mmap \
-//   ! rtspclientsink location=rtsp://localhost:8554/mystream
+  // std::string pipeline = "videotestsrc ! videoconvert ! appsink";
 
+  // Define the GStreamer pipeline
   string pipelineS =
       "v4l2src device=" + AICamrea_getVideoDevice() + " " +
       "! video/x-raw,width=640,height=480 " +
       "! v4l2h264enc extra-controls=\"cid,video_gop_size=30\" capture-io-mode=mmap "
-      "! rtspclientsink location=rtsp://localhost:8554/mystream";
+      "! appsink";
 
   xlog("pipeline:%s", pipelineS.c_str());
 
