9d94369ef150185cfecf0fce43ddbad43dcae956
Gray Lin <gray.lin@primax.com.tw>
2024-12-05 11:00:56 +0800

    z

--- test_ocv.cpp
+++ test_ocv.cpp
@@ -45,7 +45,7 @@ void ocv_test(int testCase) {
   // NG
   string pipelineS =
       "v4l2src device=" + AICamrea_getVideoDevice() + " " +
-      "! video/x-raw,width=2316,height=1746 " +
+      "! video/x-raw, width=640, height=480 " +
       "! videoconvert "
       "! appsink";
 
