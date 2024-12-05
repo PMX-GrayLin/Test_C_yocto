4185c25fbdd603e32056c2bcf6805a2320df0f21
Gray Lin <gray.lin@primax.com.tw>
2024-12-05 17:57:26 +0800

    z

--- test_gst.cpp
+++ test_gst.cpp
@@ -155,18 +155,21 @@ void gst_test2(int testCase) {
   GstBus *bus = gst_element_get_bus(pipeline);
   GstMessage *msg;
   do {
-    msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE,
-                                     (GstMessageType)(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
+    msg = gst_bus_timed_pop_filtered(
+      bus, 
+      GST_CLOCK_TIME_NONE,
+      (GstMessageType)(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
+
     if (msg != nullptr) {
       GError *err;
       gchar *debug_info;
 
       switch (GST_MESSAGE_TYPE(msg)) {
         case GST_MESSAGE_ERROR:
-          xlog("GST_MESSAGE_ERROR");
-          // gst_message_parse_error(msg, &err, &debug_info);
-          // g_error_free(err);
-          // g_free(debug_info);
+          gst_message_parse_error(msg, &err, &debug_info);
+          xlog("GST_MESSAGE_ERROR. err->message%s", err->message);
+          g_error_free(err);
+          g_free(debug_info);
           break;
 
         case GST_MESSAGE_EOS:
@@ -177,7 +180,7 @@ void gst_test2(int testCase) {
           xlog("default, Unexpected message received");
           break;
       }
-      // gst_message_unref(msg);
+      gst_message_unref(msg);
     }
   } while (msg != nullptr);
 
