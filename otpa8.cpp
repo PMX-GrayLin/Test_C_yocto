18444b0101e652fc9702fcb8df4c4adcb94db571
Gray Lin <gray.lin@primax.com.tw>
2025-02-10 19:58:08 +0800

    z

--- otpa8.cpp
+++ otpa8.cpp
@@ -23,18 +23,14 @@ bool OTPA8::readTemperature(float& ambientTemp, float& objectTemp) {
 //   uint8_t command[3] = {0xD0, 0x4E, 0x00};
 //   uint8_t command[3] = {0x68, 0x4E, 0x00};
 //   uint8_t command[2] = {0x4E, 0x00};
-  uint8_t command = 0x4E;
+  uint8_t command[1] = { 0x4E };
 
   // check
-  xlog("write...");
-//   printBuffer(command, 2);
+//   xlog("write...");
+//   printBuffer(command, 1);
 
   // Send the readout command
-//   if (write(file, command, sizeof(command)) != sizeof(command)) {
-//     xlog("Failed to send readout command");
-//     return false;
-//   }
-  if (write(file, command, 1) != 1) {
+  if (write(file, command, sizeof(command)) != sizeof(command)) {
     xlog("Failed to send readout command");
     return false;
   }
