088f42d84818702d558fadc08c5ecc1ec8ab5908
Gray Lin <gray.lin@primax.com.tw>
2025-02-13 19:37:27 +0800

    a

--- otpa8.cpp
+++ otpa8.cpp
@@ -91,7 +91,7 @@ bool OTPA8::readTemperature_max(float& ambientTemp, float& objectTemp) {
 
   // check
 //   xlog("read...");
-//   printBuffer(buffer, 141);
+  printBuffer(buffer, 141);
 
   // Parse ambient temperature (bytes 10-13)
   uint8_t ambHigh = buffer[9];  // Byte 10: AMB_H
@@ -99,6 +99,7 @@ bool OTPA8::readTemperature_max(float& ambientTemp, float& objectTemp) {
   int16_t ambientRaw = (ambHigh << 8) | ambLow;
   ambientTemp = (static_cast<float>(ambientRaw) - 27315) / 100.0f;
 
+  printf("\033[3J\033[H\033[2J");
   // Parse object temperature (bytes 14-141)
   // find Max
   float tempArray[64] = { 0.0 };
@@ -106,8 +107,14 @@ bool OTPA8::readTemperature_max(float& ambientTemp, float& objectTemp) {
   for (int i = 0; i < 64; ++i) {
     uint8_t objHigh = buffer[13 + 2 * i];  // High byte of pixel i
     uint8_t objLow = buffer[14 + 2 * i];   // Low byte of pixel i
-    int16_t objectRaw = (objHigh << 8) | objLow;
+    int16_t objectRaw = (static_cast<int16_t>(objHigh) << 8) | objLow;
     tempArray[i] = (static_cast<float>(objectRaw) - 27315) / 100.0f;
+
+    if (i % 8 == 0) {
+      printf("\n\n");
+    }
+    printf("%.3f [%X%X,]\t", tempArray[i], objHigh, objLow);
+
     if (tempArray[i] > tempMax) {
       tempMax = tempArray[i];
     }
@@ -116,7 +123,7 @@ bool OTPA8::readTemperature_max(float& ambientTemp, float& objectTemp) {
 
   // check
   // printArray_float(tempArray, 64);
-  printArray_forUI(tempArray, 64);
+  // printArray_forUI(tempArray, 64);
 
   // Log the results
   xlog("ambientTemp: %f, objectTemp: %f", ambientTemp, objectTemp);
