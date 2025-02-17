53caf9548fc33696b5c972f0d0c7866a7e1e00d5
Gray Lin <gray.lin@primax.com.tw>
2025-02-17 15:26:23 +0800

    z

--- otpa8.cpp
+++ otpa8.cpp
@@ -1,5 +1,21 @@
 #include "otpa8.hpp"
 
+float temperatureSensor[96] = { 
+  18.4, 18.4, 18.4, 18.4, 18.4, 18.4, 18.4, 18.4, 
+  18.4, 18.4, 18.4, 18.4, 18.4, 18.4, 18.4, 18.4, 
+  18.4, 18.4, 18.4, 18.4, 18.4, 18.4, 18.4, 18.4, 
+  18.4, 18.4, 18.4, 18.4, 18.4, 18.4, 18.4, 18.4, 
+  21.7, 22.3, 22.7, 24.5, 24.5, 25.5, 26.5, 27.0,
+  28.0, 28.8, 29.3, 31.0, 32.0, 32.5, 33.3, 34.8,
+  36.5, 37.0, 38.0, 38.8, 40.3, 40.7, 41.0, 43.0,
+  43.2, 43.5, 45.0, 46.0, 47.0, 47.2, 47.5, 49.0,
+  50.0, 52.0, 52.5, 53.0, 54.0, 55.0, 56.0, 57.0,
+  57.5, 58.0, 59.0, 59.5, 60.0, 61.0, 62.0, 62.3,
+  62.6, 63.0, 63.0, 63.0, 63.0, 63.0, 63.0, 63.0, 
+  63.0, 63.0, 63.0, 63.0, 63.0, 63.0, 63.0, 63.0
+ };
+
+
 OTPA8::OTPA8() {
   file = open(OTPA8_I2C_BUS, O_RDWR);
   if (file < 0) {
@@ -127,15 +143,16 @@ bool OTPA8::readTemperature_max(float& ambientTemp, float& objectTemp) {
   objectTemp = tempMax * multipler;
 
   printf("\n\n");
-  printf("multipler:%.2f", multipler);
-  printf("\n\n");
+
+  // printf("multipler:%.2f", multipler);
+  // printf("\n\n");
 
   // check
   // printArray_float(tempArray, 64);
   // printArray_forUI(tempArray, 64);
 
   // Log the results
-  printf("ambientTemp: %.2f, objectTemp: %.2f\n", ambientTemp, objectTemp);
+  printf("ambientTemp: %.2f, objectTemp: %.2f, multipler:%.2f\n", ambientTemp, objectTemp, multipler);
   // xlog("ambientTemp: %.2f, objectTemp: %.2f", ambientTemp, objectTemp);
   return true;
 }
