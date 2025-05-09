91bab35c2710212afa00e49c621265e7e6a57f0c
Gray Lin <gray.lin@primax.com.tw>
2025-05-09 20:48:36 +0800

    z

--- test_gst.cpp
+++ test_gst.cpp
@@ -301,7 +301,7 @@ void aravisTest() {
   //     "aravissrc camera-name=id1 ! videoconvert ! autovideosink";
 
   const gchar *pipeline_description =
-      "aravissrc camera-name=id1 ! videoconvert ! video/x-raw,format=NV12 ! v4l2h264enc extra-controls=cid,video_gop_size=30 capture-io-mode=dmabuf ! rtspclientsink location=rtsp://localhost:8554/mystream"
+      "aravissrc camera-name=id1 ! videoconvert ! video/x-raw,format=NV12 ! v4l2h264enc extra-controls=cid,video_gop_size=30 capture-io-mode=dmabuf ! rtspclientsink location=rtsp://localhost:8554/mystream";
   GstElement *pipeline = gst_parse_launch(pipeline_description, &error);
 
   if (!pipeline) {
