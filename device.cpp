7f15586e33091f077e20bd1444173e9058073bd7
Gray Lin <gray.lin@primax.com.tw>
2025-05-29 15:59:19 +0800

    z

--- device.cpp
+++ device.cpp
@@ -4,7 +4,7 @@
 #include <gpiod.h>
 
 // ai_camera_plus or vision_hub_plus 
-std::string product = "ai_camera_plus"
+std::string product = "ai_camera_plus";
 
 // PWM
 const std::string path_pwm = "/sys/devices/platform/soc/10048000.pwm/pwm/pwmchip0";
